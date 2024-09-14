2df3f161061faf44107856f3252108420d9d983b
Roger Jia Rong Jhang <roger225@gmail.com>
2024-09-14 23:21:03 +0800

    mcu initial

---
+++ TouchLed.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TouchLed.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TouchLed.X.production.hex
